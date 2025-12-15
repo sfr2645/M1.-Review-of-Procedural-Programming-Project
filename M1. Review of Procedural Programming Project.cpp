38b4aaac6fea4f1aa5fb56e1a4e1598b2d52038b
sfr2645 <sfr2645@email.vccs.edu>
2025-12-15 15:22:58 -0500

    Add project files.

---
+++ M1. Review of Procedural Programming Project.cpp
@@ -0,0 +1,9 @@
+// M1. Review of Procedural Programming Project.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
